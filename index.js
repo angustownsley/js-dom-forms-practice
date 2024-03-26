21295655e9977ca266ec4753f27981cb2b4754f8
Angus Townsley <angustownsley@gmail.com>
2024-03-26 10:14:58 +0000

    event listener

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+form = document.querySelector("#form")
+const inputName = document.querySelector("#name")
+const inputEmail = document.querySelector("#email")
+const list = document.querySelector("#list")
 
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
+
+    const li = document.createElement("li")
+    li.innerText = "Name: " + inputName.value + " Email: " + inputEmail.value
+
+    list.append(li)
+
+    inputName.value = inputEmail.value = ""
+})
